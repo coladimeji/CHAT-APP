63bd96ff18818a1027fa6a9f672d6478bb010510
cccrizzz <ccccrizzzz@gmail.com>
2021-10-02 16:35:37 -0400

    added server template

--- ChatTraining/server.js
+++ ChatTraining/server.js
@@ -1,90 +1,62 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
 
-// Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true,
-});
 
-const socket = io();
 
-// Join chatroom
-socket.emit('joinRoom', { username, room });
+// this is the server script
+const express = require("express")
+const app = express()
 
-// Get room and users
-socket.on('roomUsers', ({ room, users }) => {
-  outputRoomName(room);
-  outputUsers(users);
-});
+// set the template engine ejs
+app.set("view engine", "ejs")
 
-// Message from server
-socket.on('message', (message) => {
-  console.log(message);
-  outputMessage(message);
+// middlewares
+app.use(express.static("public"))
 
-  // Scroll down
-  chatMessages.scrollTop = chatMessages.scrollHeight;
-});
+// routes
+app.get("/", (req, res) => {
+  res.render("index")
+})
 
-// Message submit
-chatForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+// listen on port 3001
+const hostname = "127.0.0.1"
+const port = 3000
 
-  // Get message text
-  let msg = e.target.elements.msg.value;
+// create server
+server = app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
 
-  msg = msg.trim();
 
-  if (!msg) {
-    return false;
-  }
+// create socket.io
+const io = require("socket.io")(server)
 
-  // Emit message to server
-  socket.emit('chatMessage', msg);
+// socket.io listener
+io.on('connection', (socket) => {
+  console.log('client connected: ' + socket.id)
+  
+  // socket name defaulted to its id
+  socket.name = socket.id
 
-  // Clear input
-  e.target.elements.msg.value = '';
-  e.target.elements.msg.focus();
-});
+  // on receiving messages
+  socket.on('msg', (data) => {
+    console.log(data.msg)
+    // broadcast the new message
+    io.sockets.emit('new_msg', {username: socket.name, message: data.msg })
+  })
 
-// Output message to DOM
-function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  const p = document.createElement('p');
-  p.classList.add('meta');
-  p.innerText = message.username;
-  p.innerHTML += `<span>${message.time}</span>`;
-  div.appendChild(p);
-  const para = document.createElement('p');
-  para.classList.add('text');
-  para.innerText = message.text;
-  div.appendChild(para);
-  document.querySelector('.chat-messages').appendChild(div);
-}
+  // on receiving name changes
+  socket.on('changename', (data) => {
+    console.log(socket.name + "changed name to: " + data.newname)
+    socket.name = data.newname
+    
+  })
 
-// Add room name to DOM
-function outputRoomName(room) {
-  roomName.innerText = room;
-}
+  // on client typing 
+  socket.on('typing', (data) => {
+    // broadcast message to all the connected clients
+    socket.broadcast.emit('typing', {username: socket.name})
+  })
 
-// Add users to DOM
-function outputUsers(users) {
-  userList.innerHTML = '';
-  users.forEach((user) => {
-    const li = document.createElement('li');
-    li.innerText = user.username;
-    userList.appendChild(li);
-  });
-}
 
-//Prompt the user before leave chat room
-document.getElementById('leave-btn').addEventListener('click', () => {
-  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
-  if (leaveRoom) {
-    window.location = '../index.html';
-  } else {
-  }
-});
+
+})
+
