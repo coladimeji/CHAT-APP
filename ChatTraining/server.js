0292cd2acdda999cd7f1c28e2f388e0076abfb91
coladimeji <coladimeji@gmail.com>
2021-10-02 21:06:22 -0400

    Database interaction
    New name,  disconnect, connect, message in chat record

--- ChatTraining/server.js
+++ ChatTraining/server.js
@@ -1,9 +1,30 @@
 
+// Connnect to Mongoose
 
 
-// this is the server script
+
+
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+
+
+
+const connectionString = "mongodb://localhost:27017/admin"   
+
+// connect to mongo db
+mongoose.connect(connectionString, { useNewUrlParser: true}  )
+.then(
+  () => {console.log("Mongoose connected successfully")},                // connected
+  err => {console.log("Mongoose could not connect to database" + err)} // econnection error
+
+)
+const UEvent = require("./models/UserEvent")
+const Record =  require("./models/ChatRecord")
+
+
+
+
 
 // set the template engine ejs
 app.set("view engine", "ejs")
@@ -31,32 +52,111 @@ const io = require("socket.io")(server)
 
 // socket.io listener
 io.on('connection', (socket) => {
+  // run everytime  client connect (connection event)
   console.log('client connected: ' + socket.id)
   
   // socket name defaulted to its id
   socket.name = socket.id
+  
+  // implement user connection event push to db
+  let  newEvent  = new UEvent({
+    user_name:socket.name,
+    event:"user connected",
+    eventDesc:"user  connected to the server",
+    time:new Date().getTime()
+    
+  })
+
+
+  newEvent.save().then((result) => {
+    console.log(result)
+    
+  }).catch((err) => {
+    console.log(err)
+  })
 
   // on receiving messages
   socket.on('msg', (data) => {
     console.log(data.msg)
-    // broadcast the new message
+    // broadcast the new message to all the client
     io.sockets.emit('new_msg', {username: socket.name, message: data.msg })
+
+    // implement push chat record to db
+    let newRecord = new Record({
+      user_name:  socket.name,
+      msg:  data.msg,
+      room:0,
+      time:new Date ().getTime(),
+      socket_id: socket.id
+
+    })
+
+
+
+    newRecord.save().then((result) => {
+      console.log(result)
+      
+    }).catch((err) => {
+      console.log(err)
+    })
+
+
+
+
   })
 
   // on receiving name changes
   socket.on('changename', (data) => {
     console.log(socket.name + "changed name to: " + data.newname)
-    socket.name = data.newname
+  
     
+    // implement push user event to db
+    let newEvent = new UEvent({
+      user_name:socket.name,
+      event:  "user  channnged name",
+      eventDesc:`${socket.name} changed name to ${data.newname}`,
+      time:new Date().getTime()
+      
+    })
+      
+    newEvent.save().then((result) => {
+      console.log(result)
+      
+    }).catch((err) => {
+      console.log(err)
+    })
+
+    // user name changed
+    socket.name = data.newname
+
   })
 
   // on client typing 
-  socket.on('typing', (data) => {
-    // broadcast message to all the connected clients
-    socket.broadcast.emit('typing', {username: socket.name})
+  // socket.on('typing', (data) => {
+  //   // broadcast message to all the connected clients
+  //   socket.broadcast.emit('typing', {username: socket.name})
+  // })
+
+  socket.on("disconnect", () =>  {
+    console.log("Disconnected...")
+
+    let newEvent = new UEvent({
+      user_name:socket.name,
+      event:"user disconnected",
+      eventDesc:"user  disconnected to the server",
+      time:new Date().getTime()
+      
+    })
+      
+    newEvent.save().then((result) => {
+      console.log(result)
+      
+    }).catch((err) => {
+      console.log(err)
+    })
+  
   })
 
-
-
+    
 })
 
